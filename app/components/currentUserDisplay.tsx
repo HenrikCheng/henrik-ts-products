38daed6db1e428ef5d6be1e054edabdb9f230155
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2024-06-13 21:38:09 +0200

    fix: simplify props

--- app/components/currentUserDisplay.tsx
+++ app/components/currentUserDisplay.tsx
@@ -3,45 +3,34 @@ import { Product, User } from "./usersList";
 
 interface CurrentUserDisplayProps {
   users: User[];
-  currentUser: number | null;
-  handleUserChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  filteredProducts: Product[];
+  currentUser: string | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const CurrentUserDisplay = ({
   users,
   currentUser,
-  handleUserChange,
-  filteredProducts,
+  setCurrentUser,
 }: CurrentUserDisplayProps) => {
   return (
     <div className="gap-4 flex">
-      <div>
-        <h2>Select a user</h2>
-
-		<div className="flex gap-2">
-        {users.map((user: User) => (
-          <div key={user.id} className="flex flex-col">
-            <input
-              type="radio"
-              value={user.id}
-              name="id"
-              onChange={handleUserChange}
-            />
-            user: {user.id}
-          </div>
-        ))}
-		</div>
-      </div>
-
-      {/* <div>
-        <div>Current user: {currentUser}</div>
-        <ul>
-          {filteredProducts.map((product: Product) => (
-            <li key={product.id}>{product.title}</li>
+      <label>
+        Select a user
+        <select
+          onChange={(e) => setCurrentUser(e.target.value)}
+          value={currentUser ?? ""}
+          className="text-black"
+        >
+          <option value="" disabled>
+            Select a user
+          </option>
+          {users.map((user: User) => (
+            <option value={user.id} key={user.id}>
+              {user.id}
+            </option>
           ))}
-        </ul>
-      </div> */}
+        </select>
+      </label>
     </div>
   );
 };
--- app/components/usersList.tsx
+++ app/components/usersList.tsx
@@ -11,7 +11,7 @@ export interface Product {
 }
 
 export interface User {
-  id: number;
+  id: string;
   products: number[];
 }
 
@@ -25,39 +25,24 @@ const useProducts = () => {
 };
 
 const UsersList: React.FC = () => {
-  const [currentUser, setCurrentUser] = useState<null | number>(null);
+  const [currentUser, setCurrentUser] = useState<null | string>(null);
   const { data, error } = useProducts();
 
   if (error) return <div>Error loading data</div>;
   if (!data) return <div>Loading...</div>;
 
   const users: User[] = [
-    { id: 1, products: [1, 2] },
-    { id: 2, products: [3, 4] },
-    { id: 3, products: [1, 4, 5] },
+    { id: "1", products: [1, 2] },
+    { id: "2", products: [3, 4] },
+    { id: "3", products: [1, 4, 5] },
   ];
-
-  const handleUserChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const userId = parseInt(event.target.value);
-    setCurrentUser(userId);
-  };
-
-  const filteredProducts = currentUser
-    ? data.products.filter((product) =>
-        users
-          .find((user) => user.id === currentUser)
-          ?.products.includes(product.id)
-      )
-    : [];
-  console.log("🚀 ~ data:", data);
 
   return (
     <div className="p-4 container">
       <CurrentUserDisplay
         users={users}
         currentUser={currentUser}
-        handleUserChange={handleUserChange}
-        filteredProducts={filteredProducts}
+        setCurrentUser={setCurrentUser}
       />
 
       <div>
